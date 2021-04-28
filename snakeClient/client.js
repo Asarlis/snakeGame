02896102e53dadd5a2bed1e20899f6528e5d0d47
Sean Day <Asarlis55@gmail.com>
2021-04-28 18:00:16 +0000

    added naming function to client.js

--- snakeClient/client.js
+++ snakeClient/client.js
@@ -4,14 +4,18 @@ const connect = function () {
     host: 'localhost',
     port: 50541
   });
+  conn.write('Name: SMD')
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("data", (data) => {
     // code that does something when the connection is first established
+  
     console.log(data)
   });
-  
+  conn.on('connect', () => {
+      console.log("Successfully connected...");
+  })
   return conn;
 };
 
