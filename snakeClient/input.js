369a0d32a6890b8c1766180a5e1ff22616b70fac
Sean Day <Asarlis55@gmail.com>
2021-04-29 14:10:46 +0000

    added hardcoded canned comment function via handleUserInput function to input.js

--- snakeClient/input.js
+++ snakeClient/input.js
@@ -17,10 +17,12 @@ const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
   }
-  if (data === "w") { connection.write("Move: up")};
-  if (data === 's') { connection.write("Move: down")};
-  if (data === 'a') { connection.write("Move: left")};
-  if (data === 'd') { connection.write("Move: right")};
+
+  if (data === "w") { connection.write("Move: up")};    // moves snake up
+  if (data === 's') { connection.write("Move: down")};  // moves snake down
+  if (data === 'a') { connection.write("Move: left")};  // moves snake left
+  if (data === 'd') { connection.write("Move: right")}; // moves snake right
+  if (data === 'q') {connection.write('Say: Boom!')}
 };
 
 module.exports = {
